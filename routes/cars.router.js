cebbd4443a51e832099d68e6b6ee5e76b5ce23d8
yombi <ntwariegide2@gmail.com>
2021-05-01 08:48:07 +0200

    handling errors

--- routes/cars.router.js
+++ routes/cars.router.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.send('Get request is sent ..')
+router.get('/',async(req,res)=>{
+    try {
+        
+    } catch (err) {
+        res.send('Error : '+err)
+    }
 })
 
 module.exports = router
