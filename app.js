f06c91e803ca811aaae4b0c18efb68ddcbee7731
yombi <ntwariegide2@gmail.com>
2021-05-01 08:31:17 +0200

    working with online mongodb

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const url = "mongodb+srv://root:edaedaeda@cluster0.ulkpq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const app = express()
+
+mongoose.connect(url,
+    // working with new url
+    {useNewUrlParser: true}
+    )
+
+    //getting connection
+const connection = mongoose.connection
+
+connection.on('open', ()=>{
+    console.log('Connection to db is working .....');
+})
