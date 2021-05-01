6444ee65b86165be47927fdee7bb0e0a18ebb7db
yombi <ntwariegide2@gmail.com>
2021-05-01 08:45:22 +0200

    tested and working well

--- routes/cars.router.js
+++ routes/cars.router.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/cars',()=>{
-    console.log('Get Request is sent !!');
+router.get('/',(req,res)=>{
+    res.send('Get request is sent ..')
 })
 
 module.exports = router
