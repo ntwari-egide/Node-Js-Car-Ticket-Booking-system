90836d24737f236ee28d6d0224c4edac2a738b15
yombi <ntwariegide2@gmail.com>
2021-05-01 10:25:12 +0200

    working with cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const CarRouter = require('./routes/cars.router')
 const url = "mongodb+srv://root:edaedaeda@cluster0.ulkpq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
@@ -22,9 +23,14 @@ const connection = mongoose.connection
 
 //allowing json requests
 app.use(express.json)
+app.use(cors)
 
 //routing urls
 app.use('/cars',CarRouter)
+
+app.all('*',(req,res)=>{
+    res.send('Url is not found ...',404)
+})
 
 //listed to server
 const port = 8000
